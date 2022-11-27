dacdb9a4e2ef07ecec981cebcb67a0986c616e0c
muwl <muwl182@163.com>
2022-11-27 18:10:12 +0800

    feat:添加输出反汇编文件的功能

--- Profile/run_benchmarks.py
+++ Profile/run_benchmarks.py
@@ -17,8 +17,11 @@ class Run_Perf():
         output_file=self.config_data["subfile"]
         output_path="./"+self.config_data["paths"]["outpath"] +"/"+output_file
         self.output_perf_file=output_path+"/profile-data/"
+        self.output_disassemble_file=output_path+"/disassemble-data/"
         if (os.path.exists(self.output_perf_file))==False:
             os.system("mkdir "+self.output_perf_file)
+        if (os.path.exists(self.output_disassemble_file))==False:
+            os.system("mkdir "+self.output_disassemble_file)
     #运行具体perf命令
     def perf_command(self,app_name,app_cmd):
         stat_cmd= "perf stat -o "+self.output_perf_file+app_name  +".perf.stat.out "+ app_cmd
@@ -27,12 +30,19 @@ class Run_Perf():
         os.system(run_record)
         record_cmd= "perf report -F overhead,symbol --field-separator=,>"+self.output_perf_file+app_name+".perf.report.csv"
         os.system(record_cmd)
+    #运行objdump命令
+    def objdump_command(self,app_name,app_cmd):
+        objdump_cmd= "objdump -S "+app_cmd+">"+self.output_disassemble_file+app_name+".s"
+        os.system(objdump_cmd)
     def run(self):
         run_file1="./"+self.config_data["paths"]["benchpath"]+"/"+self.config_data["application"]["application1"]+" "+self.config_data["application"]["run_command1"]
         run_file2="./"+self.config_data["paths"]["benchpath"]+"/"+self.config_data["application"]["application2"]+" "+self.config_data["application"]["run_command2"]
         self.perf_command(self.config_data["application"]["application1"],run_file1)
         self.perf_command(self.config_data["application"]["application2"],run_file2)
         os.system("rm perf.data*")
+        self.objdump_command(self.config_data["application"]["application1"],run_file1)
+        self.objdump_command(self.config_data["application"]["application2"],run_file2)
+        
     def main(self):
         self.get_opt()
         self.run()
